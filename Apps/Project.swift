a63fd53c72db6b1cd08684499a21cc092be50ec0
alexey1312 <alexey1312ru@gmail.com>
2026-08-25 21:43:39 +0500

    chore(release): bump to 0.4.2

--- Apps/Project.swift
+++ Apps/Project.swift
@@ -28,7 +28,7 @@ let project = Project(
         base: [
             // Beta: the first tag is 0.1.0, and the tag and the shipped version
             // have to be the same number to be worth reading.
-            "MARKETING_VERSION": "0.4.1",
+            "MARKETING_VERSION": "0.4.2",
             "CURRENT_PROJECT_VERSION": "1",
         ],
         configurations: [
